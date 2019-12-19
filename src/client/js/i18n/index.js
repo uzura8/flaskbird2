0a71642e136cead70834b47e3f037e6c73a5b3be
Shingo Yamada <yamada-shingo@pal-relations.co.jp>
2019-12-19 21:01:48 +0900

    Added Thai translation

--- src/client/js/i18n/index.js
+++ src/client/js/i18n/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n';
 import trsEn from './translations/en'
 import trsJa from './translations/ja'
+import trsTh from './translations/th'
 
 Vue.use(VueI18n);
 //var locale = document.getElementsByTagName('html')[0].getAttribute('lang');
@@ -9,6 +10,7 @@ const locale = window.navigator.language
 const messages = {
   en: trsEn,
   ja: trsJa,
+  th: trsTh,
 }
 const i18n = new VueI18n({
   locale: locale,
