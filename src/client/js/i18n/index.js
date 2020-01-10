6d1021498592e924ee792ac1c1f94f89d632f93d
Shingo Yamada <yamada-shingo@pal-relations.co.jp>
2020-01-11 01:39:12 +0900

    Removed unnecessary translation

--- src/client/js/i18n/index.js
+++ src/client/js/i18n/index.js
@@ -2,15 +2,12 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n';
 import trsEn from './translations/en'
 import trsJa from './translations/ja'
-import trsTh from './translations/th'
 
 Vue.use(VueI18n);
-//var locale = document.getElementsByTagName('html')[0].getAttribute('lang');
 const locale = window.navigator.language
 const messages = {
   en: trsEn,
   ja: trsJa,
-  th: trsTh,
 }
 const i18n = new VueI18n({
   locale: locale,
